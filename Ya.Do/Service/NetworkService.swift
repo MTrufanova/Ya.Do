1b8b692afeef369cd64eae7b307252494f8addeb
Marina Trufanova <trufanovamyu@gmail.com>
2021-07-24 23:49:47 +0300

    create protocol for network Service

--- Ya.Do/Service/NetworkService.swift
+++ Ya.Do/Service/NetworkService.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+protocol NetworkServiceProtocol {
+
+    func getTasks(completion: @escaping (Result<[ToDoItem], NetworkingServiceError>) -> Void)
+    func postItem(item: ToDoItem, completion: @escaping (Result<ToDoItem, NetworkingServiceError>) -> Void )
+    // func putTasks(onResult: @escaping (Result<[NetworkingModel], Error>) -> Void)
+    func deleteItem(at id: String, completion: @escaping (Result<ToDoItem, NetworkingServiceError>) -> Void )
+    func updateItem(item: ToDoItem, completion: @escaping (Result<ToDoItem, NetworkingServiceError>) -> Void)
+}
